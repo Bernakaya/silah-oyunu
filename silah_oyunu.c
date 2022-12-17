bac95a42a29b89b3fc90df1e6ea0244ae5781cca
Bernakaya <120782114+Bernakaya@users.noreply.github.com>
2022-12-17 13:57:26 +0300

    ilk

---
+++ silah_oyunu.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int mermi = 30;
+    int secim;
+    int i = 0;
+    int sayi;
+    printf("Ateþ etmek istediðiniz mermi sayisini giriniz");
+    scanf("%d", &sayi);
+
+    for (i = 0; i <= sayi; i++)
+    {
+        if (sayi <= 30)
+            printf("pat\n");
+
+        else if (sayi = 0)
+            printf("ateþ edemez");
+
+        else
+            printf("geçerli mermi sayisi girin");
+    }
+
+    printf("Ateþ etmeye devam etmek istermisiniz\n [1] EVET \n [2] HAYÝR");
+    scanf("%d", &secim);
+    switch (secim)
+    {
+
+    case 1:
+        printf("mermi sayisi:%d\n", mermi);
+        printf("Ateþ etmek istediðiniz mermi sayisini giriniz:");
+        scanf("%d", &sayi);
+
+        if (sayi > 30)
+            printf("Geçerli mermi sayisi giriniz");
+        scanf("%d", sayi);
+
+    case 2:
+        printf("Ýþleminiz sonlandirildi");
+    }
+}
+
